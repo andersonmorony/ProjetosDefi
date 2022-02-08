385407bb8df2d6219e2fd6dbadd599fb95c2c74d
Anderson Moroni <andersonmoroni92@gmail.com>
2022-02-08 07:57:04 -0300

    add token sale with buy function

--- token_sale/contracts/DappTokenSale.sol
+++ token_sale/contracts/DappTokenSale.sol
@@ -1,15 +1,35 @@
-pragma solidity ^0.8.0;
+pragma solidity >=0.4.22;
 
 import './DappToken.sol';
+
 
 contract DappTokenSale{
     address owner;
     DappToken public tokenContract;
     uint256 public priceToken;
+    uint256 public tokenSold;
+
+    event sell(address _buyer, uint256 _amount);
 
     constructor(DappToken _tokenContract, uint256 _priceToken){
         owner = msg.sender;
         tokenContract = _tokenContract;
         priceToken = _priceToken;
+    }
+
+    function mult(uint x, uint y) internal pure returns(uint z){
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _amountOfTokens) public payable{
+        
+        require(msg.value == mult(_amountOfTokens, priceToken));
+        require(tokenContract.balanceOf(address(this)) >= _amountOfTokens);
+        require(tokenContract.transfer(msg.sender, _amountOfTokens));
+
+        tokenSold += _amountOfTokens;
+
+        emit sell(msg.sender, _amountOfTokens);
+
     }
 }
--- token_sale/test/dappTokenSale.js
+++ token_sale/test/dappTokenSale.js
@@ -1,9 +1,15 @@
-const { assert } = require("chai");
+const { assert, should } = require("chai");
 
 var DappTokenSale = artifacts.require("DappTokenSale");
+var DappTokenInstance = artifacts.require("DappToken")
 
 contract("DappTokenSale", function(accounts){
+    
     let dappTokenSale;
+    let tokenPrice = 1000000
+    let amountOfToken = 10
+    let admin = accounts[0]
+    let amountToSale = 750000
 
     it('Should has a contract address', async () => {
         dappTokenSale = await DappTokenSale.deployed()
@@ -19,5 +25,51 @@ contract("DappTokenSale", function(accounts){
 
         assert.equal(priceToken, '1000000')
     })
+
+    it('cannot buy if not have money enought', async() => {
+
+        try{
+            await dappTokenSale.buyTokens(amountOfToken, {from: accounts[1], value: 1})
+        }catch(error){
+            assert(error.message.indexOf("revert") >= 0)
+        }
+    })
+
+    it('Should have a sold token', async() => {
+
+        let dapptokenIntrance = await DappTokenInstance.deployed();
+        await dapptokenIntrance.transfer(dappTokenSale.address, amountToSale, { from: admin})
+        await dappTokenSale.buyTokens(amountOfToken, {from: accounts[1], value: amountOfToken * tokenPrice})
+
+        let sold = await dappTokenSale.tokenSold()
+
+        assert.equal(sold.toNumber(), amountOfToken, 'Should have a amountSold')
+    })
+
+    it('Should transfer 75% to token sale', async () => {
+       let dapptokenIntrance = await DappTokenInstance.deployed();
+    //    await dapptokenIntrance.transfer(dappTokenSale.address, amountToSale, { from: admin})
+       let balanceToSale = await dapptokenIntrance.balanceOf(dappTokenSale.address)
+
+       assert.isAbove(balanceToSale.toNumber(), 0)
+    })
+    it('Should return erro if not have tokens enough', async () => {
+        try{
+           await dappTokenSale.buyTokens(850000, {from: accounts[2], value:  850000 * tokenPrice})
+           let balanceSold = await dappTokenSale.tokenSold()
+           assert.isBelow(balanceSold.toNumber(), 750000)
+        }catch(error){
+            assert(error.message.indexOf("revert") >= 0, "Not have token enough")
+        }
+
+    })
+
+    it('Should to do the transfer', async () => {
+        let dapptokenIntrance = await DappTokenInstance.deployed();
+        let balanceSale = await dapptokenIntrance.balanceOf(dappTokenSale.address)
+        assert.isBelow(balanceSale.toNumber(), 750000)
+    })
+
+
 
 });
